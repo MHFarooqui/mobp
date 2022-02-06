02aaa62efc150952fb95ad0e2c17b1afcf321004
Ikramuddin Farooqui <mfarooqui@azure.uworld.com>
2022-02-06 21:00:30 +0530

    added db client and updated service methods

--- app.js
+++ app.js
@@ -18,4 +18,4 @@ app.get('/',  (req, res) => res.send({
 
 app.use(routes);
 let portNumber = process.env.PORT || 5000;
-app.listen(portNumber, () => console.log(`listning on port ${portNumber}`));
+app.listen(portNumber, () => console.log(`listning on port ${portNumber} 🤘`));
